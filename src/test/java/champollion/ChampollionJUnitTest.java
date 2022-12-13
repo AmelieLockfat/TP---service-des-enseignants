90b2da12ffa1613f4bc00b7c7832a1fc4e9f9e5f
AmelieLockFat <amelie.lock-fat@etud.univ-jfc.fr>
2022-12-13 13:56:28 +0100

    Fin

--- src/test/java/champollion/ChampollionJUnitTest.java
+++ src/test/java/champollion/ChampollionJUnitTest.java
@@ -36,5 +36,39 @@ public class ChampollionJUnitTest {
                          "L'enseignant doit maintenant avoir 30 heures prévues pour l'UE 'uml'");		
 		
 	}
-	
+
+	@Test
+	public void testHeurePrevu(){
+		//8h de TD pour le module UML
+		untel.ajouteEnseignement(uml, 0, 10, 0);
+		assertEquals(10, untel.heuresPrevues(), "L'enseignant a 8h de prévues");
+
+	}
+
+
+	@Test
+	public void testEstEnSousService(){
+
+		//Cas : en sous-service
+		//L'enseignat fait 40h d'intervention il est en dessous de 192h
+		untel.ajouteEnseignement(uml, 8, 12, 20);
+		assertTrue(untel.enSousService(), "L'enseignant doit etre en sous service");
+
+		//Cas : pas en sous-service
+		//ajout de 160h
+		untel.ajouteEnseignement(uml, 20, 36, 24 );
+		untel.ajouteEnseignement(java, 20, 24, 36);
+		assertFalse(untel.enSousService(), "L'enseignant ne doit pas être en sous service");
+
+	}
+
+	@Test
+	public void testVerifierTypeIntervention(){
+		ServicePrevu s = new ServicePrevu(uml,10, 5, 20);
+		assertTrue(s.getUeconcerne().equals(uml), "C'ets la bonne UE");
+	}
+
+
+
+
 }
